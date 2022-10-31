749a1e60541cb29d8f9612fb16aabe9801bb49c4
George Li <102538858+geo-rge-li@users.noreply.github.com>
2022-10-31 10:47:13 -0500

    add list of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Fyora' },
+  { id: 13, name: 'Darigan' },
+  { id: 14, name: 'Altador' },
+  { id: 15, name: 'Judge Hog' },
+  { id: 16, name: 'Jeran' },
+  { id: 17, name: 'Rohane' },
+  { id: 18, name: 'Nabile' },
+  { id: 19, name: 'Tormund' },
+  { id: 20, name: 'Roberta' }
+];
